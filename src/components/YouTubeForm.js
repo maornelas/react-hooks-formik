410469db94d0258551a0856e070c0427a4018b0f
Marco Ornelas <maornelasr@gmail.com>
2020-05-19 13:27:29 -0500

    Displaying error messages
    Displaying error messages

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -40,37 +40,46 @@ function YouTubeForm() {
         validate
     })
 
-    //console.log('Form. values ', formik.values)
+    //console.log('Form erros ', formik.errors)
 
     return (
         <div>
         <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="name">Name</label>
-            <input
-                type="text"
-                id="name"
-                name="name"
-                onChange={formik.handleChange}
-                value={formik.values.name}
-            />
+            <div className='form-control'>
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={formik.handleChange}
+                    value={formik.values.name}
+                />
+                {formik.errors.name && <div className="error">{formik.errors.name}</div>}
+            </div>
 
-            <label htmlFor="email">E-mail</label>
-            <input
-                type="email"
-                id="email"
-                name="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-            />
+            <div className='form-control'>
+                <label htmlFor="email">E-mail</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+                {formik.errors.email && <div className="error">{formik.errors.email}</div>}
+            </div>
 
-            <label htmlFor="channel">Channel</label>
-            <input
-                type="text"
-                id="channel"
-                name="channel"
-                onChange={formik.handleChange}
-                value={formik.values.channel}
-            />
+            <div className='form-control'>
+                <label htmlFor="channel">Channel</label>
+                <input
+                    type="text"
+                    id="channel"
+                    name="channel"
+                    onChange={formik.handleChange}
+                    value={formik.values.channel}
+                />
+                {formik.errors.channel && <div className="error">{formik.errors.channel}</div>}
+            </div>
 
             <button type="submit">Submit</button>
         </form>
