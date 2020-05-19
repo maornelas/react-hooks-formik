4e1927a260fa00066d12f2b4694a86b5c53b262c
Marco Ornelas <maornelasr@gmail.com>
2020-05-19 13:10:29 -0500

    Form Validation
    Form Validation

--- src/components/YouTubeForm.js
+++ src/components/YouTubeForm.js
@@ -1,17 +1,43 @@
 import React from 'react'
 import { useFormik } from 'formik'
 
+
+const initialValues={
+    name: '',
+    email: '',
+    channel: ''
+}
+
+const onSubmit= values => {
+    console.log('Form data ', values)
+}
+
+const  validate = values =>{
+    let errors = {}
+
+    if(!values.name){
+        errors.name = 'Required'
+    }
+
+    if(!values.email){
+        errors.email = 'Required'
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        errors.email = 'Invalid email format'
+    }
+
+    if(!values.channel){
+        errors.channel = 'Required'
+    }
+
+    return errors
+}
+
 function YouTubeForm() {
 
     const formik = useFormik({
-        initialValues:{
-            name: '',
-            email: '',
-            channel: ''
-        },
-        onSubmit: values =>{
-            console.log('Form data ', values)
-        }
+        initialValues,
+        onSubmit,
+        validate
     })
 
     //console.log('Form. values ', formik.values)
